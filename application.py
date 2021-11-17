dee6596ab61e45bb892e54d549483307aa864d1c
rafaprado3110 <53974358+rafaprado3110@users.noreply.github.com>
2021-11-17 13:12:20 -0300

    Update application.py

--- application.py
+++ application.py
@@ -1,19 +1,32 @@
+import mysql.connector
 from flask import Flask, jsonify, request
 
 application = Flask(__name__)
 
 state = {
-        "potencia": 0,
-        "data": '00/00/0000',
-        "hora": '00:00',
         "value": 0
 }
 
+potencia = {
+    "Data": "XX/XX/XXXX",
+    "Hora": "00:00",
+    "Pot": "00"
+}
 
+banco = mysql.connector.connect(host="database-2.cgnraaiqytel.us-west-2.rds.amazonaws.com", user="admin", password="usuariopi6", database="infos" )
 
 @application.route('/', methods =['GET'])
 def raiz():
     return "Bem vindo ao SX-Plug"
+
+@application.route('/Potencia', methods = ['POST'])
+def Potencia2():
+    data = request.get_json()
+    potencia.update(data)
+    bd = banco.cursor()
+    bd.execute("INSERT INTO informacoes VALUES ('" + potencia["Data"] + "', '" + potencia["Hora"] + "', " + potencia["Pot"] + ")")
+    banco.commit()
+    return ("INSERT INTO informacoes VALUES ('" + potencia["Data"] + "', '" + potencia["Hora"] + "', " + potencia["Pot"] + ")")
 
 @application.route('/PegaStatus', methods =['GET'])
 def GetState():
