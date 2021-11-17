9036bab7df0fd18931c23535c3a5a323fb122d73
rafaprado3110 <53974358+rafaprado3110@users.noreply.github.com>
2021-11-17 14:38:50 -0300

    Update application.py

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-#import mysql.connector
+import mysql.connector
 from flask import Flask, jsonify, request
 
 application = Flask(__name__)
@@ -13,7 +13,7 @@ potencia = {
     "Pot": "00"
 }
 
-#banco = mysql.connector.connect(host="database-2.cgnraaiqytel.us-west-2.rds.amazonaws.com", user="admin", password="usuariopi6", database="infos" )
+banco = mysql.connector.connect(host="database-2.cgnraaiqytel.us-west-2.rds.amazonaws.com", user="admin", password="usuariopi6", database="infos" )
 
 @application.route('/', methods =['GET'])
 def raiz():
@@ -23,9 +23,9 @@ def raiz():
 def Potencia2():
     data = request.get_json()
     potencia.update(data)
-    #bd = banco.cursor()
-    #bd.execute("INSERT INTO informacoes VALUES ('" + potencia["Data"] + "', '" + potencia["Hora"] + "', " + potencia["Pot"] + ")")
-    #banco.commit()
+    bd = banco.cursor()
+    bd.execute("INSERT INTO informacoes VALUES ('" + potencia["Data"] + "', '" + potencia["Hora"] + "', " + potencia["Pot"] + ")")
+    banco.commit()
     return ("INSERT INTO informacoes VALUES ('" + potencia["Data"] + "', '" + potencia["Hora"] + "', " + potencia["Pot"] + ")")
 
 @application.route('/PegaStatus', methods =['GET'])
