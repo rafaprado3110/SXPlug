b5252747b2a79dfeb22a363751e449b6e5c4b348
rafaprado3110 <53974358+rafaprado3110@users.noreply.github.com>
2021-11-17 13:15:32 -0300

    Update application.py

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-import mysql.connector
+#import mysql.connector
 from flask import Flask, jsonify, request
 
 application = Flask(__name__)
@@ -23,9 +23,9 @@ def raiz():
 def Potencia2():
     data = request.get_json()
     potencia.update(data)
-    bd = banco.cursor()
-    bd.execute("INSERT INTO informacoes VALUES ('" + potencia["Data"] + "', '" + potencia["Hora"] + "', " + potencia["Pot"] + ")")
-    banco.commit()
+    #bd = banco.cursor()
+    #bd.execute("INSERT INTO informacoes VALUES ('" + potencia["Data"] + "', '" + potencia["Hora"] + "', " + potencia["Pot"] + ")")
+    #banco.commit()
     return ("INSERT INTO informacoes VALUES ('" + potencia["Data"] + "', '" + potencia["Hora"] + "', " + potencia["Pot"] + ")")
 
 @application.route('/PegaStatus', methods =['GET'])
